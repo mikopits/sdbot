27c069d3c2ffd7858cf71f9d4976527b79a1759c
Eric Furugori <mikopits@gmail.com>
2016-05-24 04:16:34 -0700

    Committing merged changes in abandoned battle branch

--- handlers.go
+++ handlers.go
@@ -87,10 +87,9 @@ func onJoin(msg *Message) {
 
 func onNick(m *Message) {
 	oldNick := Sanitize(m.Params[0])
+	Rename(oldNick, m.User.Name, m.Bot)
 	if oldNick == Sanitize(m.Bot.Nick) {
 		m.Bot.Nick = m.User.Name
-	} else {
-		Rename(oldNick, m.User.Name, m.Bot)
 	}
 }
 
